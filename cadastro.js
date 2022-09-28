3234ec9281147ad523a0a0be46c0de88ec321b64
EdcarlosDev <edcarlosas2@yahoo.com.br>
2022-09-28 09:15:42 -0300

    Adicionando funcao de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,9 @@
 function signup(email){
     return email;
 }
-
 //pegando um e-mail e cadastrando na plataforma
+
+function pegaEmail(){
+    return 'Bem Vindo';
+}
+//Adicionando função de mensagem
